f4ccba79464244fecf17f528f5493447297516ab
Michał Janiczko <michaljaniczko1@gmail.com>
2019-06-10 19:46:42 +0200

    Add Profiles and single profile routes

--- src/App.js
+++ src/App.js
@@ -12,6 +12,8 @@ import EditProfile from './components/profile-forms/EditProfile';
 import AddExperience from './components/profile-forms/AddExperience';
 import AddEducation from './components/profile-forms/AddEducation';
 import PrivateRoute from './components/routing/PrivateRoute';
+import Profiles from './components/profiles/Profiles';
+import Profile from './components/profile/Profile';
 import './App.css';
 
 // Redux
@@ -40,6 +42,8 @@ const App = () => {
             <Switch>
               <Route exact path='/register' component={Register} />
               <Route exact path='/login' component={Login} />
+              <Route exact path='/profiles' component={Profiles} />
+              <Route exact path='/profile/:id' component={Profile} />
               <PrivateRoute exact path='/dashboard' component={Dashboard} />
               <PrivateRoute
                 exact
