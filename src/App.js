334a7d0900a00de81af8babee5aa1e4f1f4fdc4d
Michał Janiczko <michaljaniczko1@gmail.com>
2019-06-12 15:59:32 +0200

    Add single post component

--- src/App.js
+++ src/App.js
@@ -15,6 +15,7 @@ import PrivateRoute from './components/routing/PrivateRoute';
 import Profiles from './components/profiles/Profiles';
 import Profile from './components/profile/Profile';
 import Posts from './components/posts/Posts';
+import Post from './components/post/Post';
 import './App.css';
 
 // Redux
@@ -67,6 +68,7 @@ const App = () => {
                 component={AddEducation}
               />
               <PrivateRoute exact path='/posts' component={Posts} />
+              <PrivateRoute exact path='/posts/:id' component={Post} />
             </Switch>
           </section>
         </Fragment>
