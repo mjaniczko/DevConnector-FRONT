f0f852bbd261727837484a3b430e56ef4e15c828
Michał Janiczko <michaljaniczko1@gmail.com>
2019-06-12 16:05:24 +0200

    Add singular post component

---
+++ src/components/post/Post.js
@@ -0,0 +1,40 @@
+import React, { Fragment, useEffect } from 'react';
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { getPost } from '../../actions/post';
+import Spinner from '../layout/Spinner';
+import PostItem from '../posts/PostItem';
+import CommentForm from './CommentForm';
+
+const Post = ({ getPost, post: { post, loading }, match }) => {
+  useEffect(() => {
+    getPost(match.params.id);
+  }, [getPost]);
+
+  return loading || post === null ? (
+    <Spinner />
+  ) : (
+    <Fragment>
+      <Link to='/posts' className='btn'>
+        Back To Posts
+      </Link>
+      <PostItem post={post} showActions={false} />
+      <CommentForm postId={post._id} />
+    </Fragment>
+  );
+};
+
+Post.propTypes = {
+  getPost: PropTypes.func.isRequired,
+  post: PropTypes.object.isRequired
+};
+
+const mapStateToProps = state => ({
+  post: state.post
+});
+
+export default connect(
+  mapStateToProps,
+  { getPost }
+)(Post);
